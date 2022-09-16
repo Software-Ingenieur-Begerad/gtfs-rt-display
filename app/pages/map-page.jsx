3f5a901560f202fdc6712ad1d9df80fced8f4163
Begerad, Stefan <dialog@swingbe.de>
2022-09-16 14:24:10 +0200

    feat: add periodic interval using useEffect hook

--- app/pages/map-page.jsx
+++ app/pages/map-page.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import Map from '../components/map/map';
 export default function MapPage() {
+    useEffect(()=>{
+	const intervalCall=setInterval(()=>{
+	    console.log('get data');
+	}, 30000);
+	return ()=>{
+	    /*clean up*/
+	    clearInterval(intervalCall);
+	};
+    },[]);
     return (
 	<>
 	    {/*TODO remove debugging*/}
