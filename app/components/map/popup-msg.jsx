2d39e44fd2f3c250f8770b43ce53fb09508f2cc7
Begerad, Stefan <dialog@swingbe.de>
2022-09-30 11:39:27 +0200

    format: adjust time values in popup

--- app/components/map/popup-msg.jsx
+++ app/components/map/popup-msg.jsx
@@ -6,9 +6,22 @@ import seconds2dmhs from '../../utils/seconds2dhms';
 import getRouteType from '../../utils/gtfs-route-type';
 
 const PopupMsg = ({index,message,ptByIfleet}) => {
-    const vehicleAge=seconds2dmhs(Math.round(((Date.now()-message.tsMsgCreationVehicle*1000)/1000).toFixed(0)));
-    const itcsAge=seconds2dmhs(Math.round(((Date.now()-message.tsMsgCreationItcs*1000)/1000).toFixed(0)));
-    const clientAge=seconds2dmhs(Math.round(((Date.now()-message.tsMsgReception*1000)/1000).toFixed(0)));
+    /*get number of ms since epoch*/
+    const nowTsMs=Date.now();
+    const nowTs=Math.round(nowTsMs/1000);
+    const nowAge=seconds2dmhs(Math.round(nowTs-nowTs));
+    const nowDate=new Date(nowTsMs);
+    const nowString=nowDate.toString();
+    const itcsTs=message.tsMsgCreationItcs;
+    const itcsTsMs=itcsTs*1000;
+    const itcsAge=seconds2dmhs(Math.round(nowTs-itcsTs));
+    const itcsDate=new Date(itcsTsMs);
+    const itcsString=itcsDate.toString()
+    const clientTs=message.tsMsgReception;
+    const clientTsMs=clientTs*1000;
+    const clientAge=seconds2dmhs(Math.round(nowTs-clientTs));
+    const clientDate=new Date(clientTsMs);
+    const clientString=clientDate.toString();
     return (
 	<>
 	<Popup
@@ -17,9 +30,16 @@ const PopupMsg = ({index,message,ptByIfleet}) => {
 	>
 	    id: {message.id} <br/>
 	    vehicle id: {message.vehicleId} <br/>
-	    creation time at vehicle: ts:{message.tsMsgCreationVehicle}, age:{vehicleAge} <br/>
-	    creation time at ITCS: ts:{message.tsMsgCreationItcs}, age:{itcsAge} <br/>
-	    reception time at client: ts:{message.tsMsgReception}, age:{clientAge} <br/>
+	    now: ts(ms):{nowTsMs} <br/>
+	    now: ts(s):{nowTs} <br/>
+	    now: date:{nowString} <br/>
+	    now: age:{nowAge} <br/>
+	    creation time at ITCS: ts:{itcsTs} <br/>
+	    creation time at ITCS: date:{itcsString} <br/>
+	    creation time at ITCS: age:{itcsAge} <br/>
+	    reception time at client: ts:{clientTs} <br/>
+	    reception time at client: date:{clientString} <br/>
+	    reception time at client: age:{clientAge} <br/>
 	    trip id: {ptByIfleet.trip_id} <br/>
 	    trip name: {ptByIfleet.trip_short_name} <br/>
 	    trip destination: {ptByIfleet.trip_headsign} <br/>
